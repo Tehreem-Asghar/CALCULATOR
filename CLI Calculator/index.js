779038b387ec577e306db51d4f38a5bbea864611
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-03-20 02:54:00 +0500

    CLI Calculator

--- CLI Calculator/index.js
+++ CLI Calculator/index.js
@@ -1,39 +1,58 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter 2nd Number", type: "number", name: "secondNumber" },
-    {
-        message: "Select one of the  operator to perform Operation",
-        type: "list",
-        name: "operator",
-        choices: [
-            "Addition",
-            "subtraction",
-            "multiplication",
-            "Divison",
-            "modules",
-            "exponentiation",
-        ],
-    },
-]);
-if (answer.operator === "Addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
+import chalk from "chalk";
+async function calculator() {
+    const answer = await inquirer.prompt([
+        { message: "Enter first number", type: "number", name: "firstNumber" },
+        { message: "Enter 2nd Number", type: "number", name: "secondNumber" },
+        {
+            message: "Select one of the  operator to perform Operation",
+            type: "list",
+            name: "operator",
+            choices: [
+                "Addition",
+                "subtraction",
+                "multiplication",
+                "Divison",
+                "modules",
+                "exponentiation",
+            ],
+        },
+    ]);
+    if (answer.operator === "Addition") {
+        console.log(chalk.bgGreen.white.bold(answer.firstNumber + answer.secondNumber));
+    }
+    else if (answer.operator === "subtraction") {
+        console.log(chalk.bgYellow.bold.blue(answer.firstNumber - answer.secondNumber));
+    }
+    else if (answer.operator === "multiplication") {
+        console.log(chalk.bgCyan.gray.bold(answer.firstNumber * answer.secondNumber));
+    }
+    else if (answer.operator === "Divison") {
+        console.log(chalk.bgGreenBright.white.bold(answer.firstNumber / answer.secondNumber));
+    }
+    else if (answer.operator === "modules") {
+        console.log(chalk.bgBlueBright.magenta.bold(answer.firstNumber % answer.secondNumber));
+    }
+    else if (answer.operator === "exponentiation") {
+        console.log(chalk.bgRedBright.black.bold(answer.firstNumber ** answer.secondNumber));
+    }
+    else {
+        console.log("pleas select valid operator.");
+    }
+    let again = await inquirer.prompt([
+        {
+            message: "Do you Want To perform Another Calculation?",
+            type: "list",
+            name: "Again",
+            choices: ["Yes", "No"],
+        },
+    ]);
+    if (again.Again === "Yes") {
+        calculator();
+    }
+    else {
+        console.log("Goodbye!");
+    }
 }
-else if (answer.operator === "subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operator === "multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operator === "Divison") {
-    console.log(answer.firstNumber / answer.secondNumber);
-}
-else if (answer.operator === "modules") {
-    console.log(answer.firstNumber % answer.secondNumber);
-}
-else if (answer.operator === "exponentiation") {
-    console.log(answer.firstNumber ** answer.secondNumber);
-}
-else {
-    console.log("pleas select valid operator.");
-}
+calculator();
